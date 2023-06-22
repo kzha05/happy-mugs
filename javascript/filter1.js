60c0cc1938c86427fb73f6a527f26dd822948d91
diya jawalapersad <34486@ma-web.nl>
2023-06-22 13:47:41 +0200

    added first filter

--- javascript/filter1.js
+++ javascript/filter1.js
@@ -1,3 +1,75 @@
 let allMugs = document.getElementsByClassName("card");
 
-console.log(allMugs);
+let filters = document.getElementsByClassName("filter");
+//food inspired
+let foodfilter = document.getElementById("checkbox-foodinspired")
+foodfilter.onchange = function(){
+    if(foodfilter.checked === true){
+        for(let i = 0; i < allMugs.length; i++){
+            if(allMugs[i].dataset.category === "food inspired"){
+                allMugs[i].style.display = "block";
+            }
+        }
+    }
+    else{
+        for(let i = 0; i < allMugs.length; i++){
+            if(allMugs[i].dataset.category === "food inspired"){
+                allMugs[i].style.display = "none";
+            }
+        }
+    }
+}
+
+let animalfilter = document.getElementById("checkbox-animal")
+animalfilter.onchange = function(){
+    if(animalfilter.checked === true){
+        for(let i = 0; i < allMugs.length; i++){
+            if(allMugs[i].dataset.category === "animal"){
+                allMugs[i].style.display = "block";
+            }
+        }
+    }
+    else{
+        for(let i = 0; i < allMugs.length; i++){
+            if(allMugs[i].dataset.category === "animal"){
+                allMugs[i].style.display = "none";
+            }
+        }
+    }
+}
+
+let basicfilter = document.getElementById("checkbox-basic")
+basicfilter.onchange = function(){
+    if(basicfilter.checked === true){
+        for(let i = 0; i < allMugs.length; i++){
+            if(allMugs[i].dataset.category === "basic"){
+                allMugs[i].style.display = "block";
+            }
+        }
+    }
+    else{
+        for(let i = 0; i < allMugs.length; i++){
+            if(allMugs[i].dataset.category === "basic"){
+                allMugs[i].style.display = "none";
+            }
+        }
+    }
+}
+
+let otherfilter = document.getElementById("checkbox-other")
+otherfilter.onchange = function(){
+    if(otherfilter.checked === true){
+        for(let i = 0; i < allMugs.length; i++){
+            if(allMugs[i].dataset.category === "other"){
+                allMugs[i].style.display = "block";
+            }
+        }
+    }
+    else{
+        for(let i = 0; i < allMugs.length; i++){
+            if(allMugs[i].dataset.category === "other"){
+                allMugs[i].style.display = "none";
+            }
+        }
+    }
+}
--- products1.php
+++ products1.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
-    <script src="javascript/filter1.js"></script>
+    <script src="javascript/filter1.js" defer></script>
     <title>happy mugs</title>
 </head>
 <body>
